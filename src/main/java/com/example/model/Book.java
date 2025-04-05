9f2a2a2d7da70422a9b43a8a37640d1781e49e44
Vladyslav Panasyuk <panasyuk.vladyslav.kpi@gmail.com>
2025-04-06 00:07:14 +0300

    feat: override the equals and hashCode methods in the Book class

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -1,5 +1,7 @@
 package com.example.model;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private String author;
@@ -62,5 +64,26 @@ public class Book {
             throw new IllegalStateException("The book has not been borrowed and cannot be given back.");
         }
         isBorrowed = false;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Book book = (Book) obj;
+        return publicationYear == book.publicationYear &&
+           Objects.equals(title, book.title) &&
+           Objects.equals(author, book.author) &&
+           Objects.equals(publisher, book.publisher);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publisher, publicationYear);
     }
 }
